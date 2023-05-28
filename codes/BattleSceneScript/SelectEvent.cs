dfdd0b45e75e09d0a6c65ebf98f215b8916493c0
Nkemw <92009396+Nkemw@users.noreply.github.com>
2023-05-28 09:35:10 +0900

    Update SelectEvent.cs

--- codes/BattleSceneScript/SelectEvent.cs
+++ codes/BattleSceneScript/SelectEvent.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class SelectEvent : MonoBehaviour
 {
-    //[SerializeField] GameObject controller;
-
     private void OnMouseDown()
     {
-        /*if (controller.gameObject.TryGetComponent<BattleController>(out BattleController con))
-        {
-            con.MonsterDamaged(this.gameObject);
-        }*/
-
         BattleController.Instance.isPlayerAttack = !BattleController.Instance.isPlayerAttack;
         BattleController.Instance.MonsterDamaged(this.gameObject);
         
